ff0690c2b9ab817c6e6247faae19abc96e628f48
satya sindhu <movva001@gmail.com>
2021-11-23 20:55:38 -0500

    modified eqArrays.js

--- head.js
+++ head.js
@@ -1,14 +1,14 @@
 const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log("✅" + `Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log("🔴" + `Assertion Failed: ${actual} !== ${expected}`);
-    }
-  };
-  const head = function(input_value) {
-    head_value = input_value[0];
-    //console.log(head_value)
-    return head_value
-  };
+  if (actual === expected) {
+    console.log("✅" + `Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log("🔴" + `Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head = function(inputValue) {
+  head_value = inputValue[0];
+  //console.log(headvalue)
+  return head_value;
+};
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
