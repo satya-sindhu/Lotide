2b40df6fc162e903a481b1ec893b43b3f72d2f6c
satya sindhu <movva001@gmail.com>
2021-11-24 22:29:20 -0500

    added eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -20,27 +20,21 @@ const assertEqual = function(actual, expected) {
     }
   }
 
-  const eqObjects = (obj1, obj2) => {
-    const obj1Keys = Object.keys(obj1);
-    const obj2Keys = Object.keys(obj2);
-  
-    if (obj1Keys.length === obj2Keys.length) {
-      for (let value in obj1) {
-        if (Array.isArray(obj1[value]) && Array.isArray(obj2[value])) {
-          if (eqArrays(obj1[value], obj2[value]) === false) {
-            return false;
-          }
-        } else {
-          if (obj1[value] !== obj2[value]) {
-            return false;
-          }
+  const eqObjects = function(object1, object2) { 
+    if (Object.keys(object1).length !== Object.keys(object2).length) {
+        return false;
+      }
+    
+      for (obj1 of Object.keys(object1)) {
+        if (object1[obj1].isArray === true && object2[obj1].isArray === true){
+          if (eqArrays(object1[obj1], object2[obj1] === false))
+            return false
+        } else if (object1[obj1] !== object2[obj1]) {
+          return false;
         }
       }
-    } else {
-      return false;
-    }
-    return true;
-  };
+      return true;
+};
   
 // TEST
 const ab = { a: "1", b: "2" };
