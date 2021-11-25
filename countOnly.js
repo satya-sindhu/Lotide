607f58c7293ac5a8d6293e79fc02dd3590cb1dbc
satya sindhu <movva001@gmail.com>
2021-11-24 20:16:08 -0500

    added countLetters.js

--- countOnly.js
+++ countOnly.js
@@ -11,7 +11,7 @@ const assertEqual = function(actual, expected) {
 const countOnly = function(allItems, itemsToCount) {
     const results = {};
     for (const item of allItems) {
-        console.log(item);
+        // console.log(item);
         if (itemsToCount[item]) {
             if (results[item]) {
                 results[item] += 1;
@@ -20,7 +20,6 @@ const countOnly = function(allItems, itemsToCount) {
             }
         }
     }
-    //console.log(results)
   return results;
 
 }
