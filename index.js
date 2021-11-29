cff3e25ce22e4dec951b419faaaaeb19b929a489
satya sindhu <movva001@gmail.com>
2021-11-29 16:22:52 -0500

    changed index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
+const map = require('./map');
+const letterPositions = require('./letterPositions');
+const findKeyByValue = require('./findKeyByValue');
+const findKey = require('./findKey');
+const  eqObjects = require('./eqObjects');
+const  eqArrays = require('./eqArrays');
+const  countOnly = require('./countOnly');
+const  countLetters = require('./countLetters');
+const  assertEqual = require('./assertEqual');
+const  assertArraysEqual = require('./assertArraysEqual');
+
+
+
+module.exports = {
+    head,
+    tail,
+    middle,
+    without,
+    takeUntil,
+    map,
+    letterPositions,
+    findKeyByValue,
+    findKey,
+    eqObjects,
+    eqArrays,
+    countOnly,
+    countLetters,
+    assertEqual,
+    assertArraysEqual
+};
