947d5573df587f9bb722769b2e7d8ca9741595cf
satya sindhu <movva001@gmail.com>
2021-11-29 12:43:41 -0500

    refactored func

--- head.js
+++ head.js
@@ -1,14 +1,23 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("✅" + `Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log("🔴" + `Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-const head = function(inputValue) {
-  head_value = inputValue[0];
-  //console.log(headvalue)
-  return head_value;
-};
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+const assertEqual = require('./assertEqual');
+
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log("✅" + `Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log("🔴" + `Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+const head = function (arr) {
+  let x = 0;
+  x = arr.shift();
+  console.log(x);
+  return x;
+}
+ 
+
+// TEST CODE
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+module.exports = head;
